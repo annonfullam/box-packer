18266b5eeea11149e6b447551e2c3469faeb3a56
kyfex-uwu <retrogrape117@gmail.com>
2024-11-26 12:06:13 -0800

    packable and pack_scene now are working
    packables now can be inside the area, and only when all packables are inside the box does the scene win (console print)

--- _scripts/pack_scene.gd
+++ _scripts/pack_scene.gd
@@ -1,11 +1,25 @@
 extends Node3D
 class_name PackScene
 
+var selected: Packable
+@onready var box_area: Area3D = $BoxArea
+@onready var fence_area: Area3D = $FenceArea
 
 func _ready() -> void:
 	for child: Packable in $Packables.get_children():
 		child.register_in_scene(self)
+		child.selected.connect(func(): change_selected(child))
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
+func check_all_in() -> void:
+	for child: Packable in $Packables.get_children():
+		if !child.inside(): return
+	print("level won")
+
+func change_selected(p_selected: Packable) -> void:
+	if selected==p_selected: return
+	
+	if selected: selected.deselected.emit()
+	selected=p_selected
--- _scripts/packable.gd
+++ _scripts/packable.gd
@@ -2,13 +2,39 @@ extends Node3D
 class_name Packable
 
 var scene: PackScene
-func register_in_scene(p_scene: PackScene):
+var _in_box: bool = false
+var _in_fence: bool = false
+signal selected
+signal deselected
+
+func inside() -> bool: return !_in_fence && _in_box
+
+@onready var rigidbody: RigidBody3D = $RigidBody3D
+func register_in_scene(p_scene: PackScene) -> void:
 	scene=p_scene
 	
+	scene.box_area.body_entered.connect(func(body: Node3D):
+		if body != rigidbody: return
+		_in_box=true
+		scene.check_all_in())
+	scene.fence_area.body_exited.connect(func(body: Node3D):
+		if body != rigidbody: return
+		_in_fence=false
+		scene.check_all_in())
+	scene.box_area.body_exited.connect(func(body: Node3D):
+		if body != rigidbody: return
+		_in_box=false)
+	scene.fence_area.body_entered.connect(func(body: Node3D):
+		if body != rigidbody: return
+		_in_fence=true)
+
 func on_click(camera: Camera3D, event: InputEvent, event_position: Vector3, 
 		normal: Vector3, shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-		print("clicked on me!")
+		selected.emit()
 		
 func _ready() -> void:
-	$RigidBody3D.connect("input_event", on_click)
+	rigidbody.connect("input_event", on_click)
+	
+func _process(delta: float) -> void:
+	pass
