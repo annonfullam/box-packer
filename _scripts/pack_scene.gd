38c5b71e5230f85c41d0a3074daec885da1b02bf
kyfex-uwu <retrogrape117@gmail.com>
2024-11-25 21:19:23 -0800

    added to packscene

--- _scripts/pack_scene.gd
+++ _scripts/pack_scene.gd
@@ -6,7 +6,6 @@ func _ready() -> void:
 	for child: Packable in $Packables.get_children():
 		child.register_in_scene(self)
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
