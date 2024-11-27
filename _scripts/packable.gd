c1dd526c0d72c96a13481ddd5c3d12fa0fdf0e55
kyfex-uwu <retrogrape117@gmail.com>
2024-11-26 20:25:44 -0800

    shader on select

--- _scripts/packable.gd
+++ _scripts/packable.gd
@@ -4,15 +4,10 @@ class_name Packable
 var is_selected: bool = false
 
 signal selected
-@warning_ignore("unused_signal") signal deselected
+signal deselected
 
 @onready var rigidbody: RigidBody3D = $".."
 @export var mesh_to_highlight: MeshInstance3D
-
-func _ready() -> void:
-	assert(mesh_to_highlight.mesh.material != null, "there's a packable mesh without a material :(")
-	# this might break when we add custom meshes
-	mesh_to_highlight.mesh.material.next_pass = Global.packable_highlight_shader
 
 func register_in_scene(manager: GameManager) -> void:
 	manager.box_area.body_entered.connect(func(body: Node3D):
@@ -29,6 +24,15 @@ func register_in_scene(manager: GameManager) -> void:
 	manager.fence_area.body_entered.connect(func(body: Node3D):
 		if body != rigidbody: return
 		_in_fence = true)
+		
+func _ready() -> void:
+	assert(mesh_to_highlight.mesh.material != null, "there's a packable mesh without a material :(")
+	# this might break when we add custom meshes
+	selected.connect(func(_unused):
+		mesh_to_highlight.mesh.material.next_pass =\
+		Global.packable_highlight_shader)
+	deselected.connect(func(_unused):
+		mesh_to_highlight.mesh.material.next_pass = null)
 
 
 var _in_box: bool = false
@@ -40,4 +44,3 @@ func _input(event: InputEvent) -> void:
 		var hit: Dictionary = Global.cursor_raycast()
 		if hit.has("collider"):
 			selected.emit(hit)
-			print("selected!")
